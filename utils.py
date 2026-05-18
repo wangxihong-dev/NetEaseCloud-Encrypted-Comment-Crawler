ed676f01dbbe1bc8468c4114851cabbe5951d162
wangxihong-dev <3408027962@qq.com>
2026-05-18 22:03:15 +0800

    更新README：修正截图文件名，完善项目运行说明

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ def ensure_output_dir():
 
 def save_comments(comment_list,song_id):
     """将评论保存到本地文本文件"""
-    filename = f"output/歌曲_{song_id}_评论.txt"
+    filename = f"output/歌曲_{song_id}.txt"
 
     with open(filename, 'w', encoding='utf-8') as f:
         for index, comment in enumerate(comment_list, start=1):
